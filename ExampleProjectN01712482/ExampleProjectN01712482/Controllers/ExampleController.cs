bd8f7690309328e6d6d2973c40f234d39a620cd7
Joyce Lin <ohanalin@gmail.com>
2024-09-22 08:43:11 -0400

    update

---
+++ ExampleProjectN01712482/ExampleProjectN01712482/Controllers/ExampleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleProjectN01712482.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        // This is a code coment!
+        //GOAL :
+        // GET:localhost:XX/api/example -> "hello world"
+        [HttpGet(template:"method1")]
+        public string Method1() 
+        {
+            return "Hello World!";
+        }
+        // GOAL:
+        //GET: local host:xx/api/example2-->"Hello 5125"
+        [HttpGet(template:"method2")]
+
+        public string Method2()
+        {
+            return "Hello 5125";
+        }
+        [HttpPost(template:"method3")]
+        public string Method3() 
+        {
+            return "A Post Request";
+        }
+
+    }
+}
