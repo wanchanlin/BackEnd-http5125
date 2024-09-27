0b8bf0e3090882e057fc75c993a220d67555eacb
Joyce Lin <ohanalin@gmail.com>
2024-09-27 18:40:57 -0400

    update

---
+++ Assgnment1/CSharp-Assigment1/WebApplication1/Controllers/q4Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class q4Controller : ControllerBase
+    {   /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost(template: "knockknock")]
+        public string knockknock()
+        {
+            return "Who's there?";
+            
+        }
+
+    }
+}
